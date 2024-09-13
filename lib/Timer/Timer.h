70c9587b2bd405b5b5b2b67de9b4c2e7ec034a5f
Youssef <elkattoufi.youssef@gmail.com>
2024-09-13 20:56:10 +0200

    Mise en place d'un seul timer

---
+++ lib/Timer/Timer.h
@@ -0,0 +1,11 @@
+#include <Arduino.h>
+
+#ifndef _Timer_H
+#define _Timer_H
+
+
+
+void onTime();
+void timer_init(int numero_timer,float reglage_alarm_timer_seconde);
+
+#endif
--- src/main.cpp
+++ src/main.cpp
@@ -1,35 +1,19 @@
 #include <Arduino.h>
-#include "MOTEUR.h"
-#include "EncoderManager.h"
-float Te = 10;
-moteur moteurDroit;
-moteur moteurGauche;
+#include "Timer.h"
+extern volatile bool timerFlag ;  // Flag déclenché lors de l'interruption
 
-// Définition des pins pour les encodeurs gauche et droit
-const int pinRightA = 33;
-const int pinRightB = 32;
 
-// Création d'une instance de la classe EncoderManager
-EncoderManager roue_droit(33, 32);
-EncoderManager roue_gauche(26, 25);
-
-void setup()
-{
+void setup() {
     Serial.begin(115200);
-    moteurDroit.init(19, 18, 0, 19500, 12, "Moteur Droit");
-    moteurGauche.init(16, 17, 2, 19500, 12, "Moteur Gauche");
-
-    roue_droit.init("roue droite", 65, 712); // Initialiser les encodeurs
-        roue_gauche.init("roue gauche", 65, 712); // Initialiser les encodeurs
-
+  timer_init(0, 10);
 }
 
-void loop()
-{
-    moteurGauche.setSpeed(100);
-    moteurDroit.setSpeed(100);
-    roue_droit.showTickPosition();
-    roue_gauche.showTickPosition();
-    delay(1000);
+void loop() {
+  // Si le flag du timer est déclenché, effectuer une action
+  if (timerFlag) {
+    Serial.println("10 secondes s'est écouler");
+    timerFlag = false;  // Réinitialiser le flag
+  }
 
+  // Autres tâches possibles dans la boucle principale
 }
